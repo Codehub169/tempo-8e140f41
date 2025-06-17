1ec5f63059503769765dfc32afe1c8d754812863
Codehub169 <codehub@169pi.com>
2025-06-17 18:00:50 +0530

    Applied user request: i am getting many errors "Uncaught Error: You cannot render a <Router> inside another <Router>. You should never have more than one in your app.    invariant history.ts:494    Router components.tsx:42

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { Box, Flex } from '@chakra-ui/react';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -12,27 +12,25 @@ import ArticlePage from './pages/ArticlePage';
 
 function App() {
   return (
-    <Router>
-      <Flex direction="column" minH="100vh" bg="brand.bg"> {/* Using brand.bg from Chakra theme */}
-        <Navbar />
-        {/* Main content wrapper div */}
-        <Box as="main" flexGrow={1} pt="nav"> {/* 'nav' should be a theme token for Navbar height */}
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/history" element={<HistoryPage />} />
-            <Route path="/differences" element={<DifferencesPage />} />
-            <Route path="/usage" element={<UsagePage />} />
-            <Route path="/article/:articleId" element={<ArticlePage />} />
-            {/* A generic article route for linking from non-dynamic parts if needed, or for a default article */}
-            <Route path="/article" element={<ArticlePage />} /> 
-            {/* <Route path="/about" element={<AboutPage />} /> */}
-            {/* Consider adding a 404 Not Found page later */}
-            {/* <Route path="*" element={<NotFoundPage />} /> */}
-          </Routes>
-        </Box>
-        <Footer />
-      </Flex>
-    </Router>
+    <Flex direction="column" minH="100vh" bg="brand.bg"> {/* Using brand.bg from Chakra theme */}
+      <Navbar />
+      {/* Main content wrapper div */}
+      <Box as="main" flexGrow={1} pt="nav"> {/* 'nav' should be a theme token for Navbar height */}
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/history" element={<HistoryPage />} />
+          <Route path="/differences" element={<DifferencesPage />} />
+          <Route path="/usage" element={<UsagePage />} />
+          <Route path="/article/:articleId" element={<ArticlePage />} />
+          {/* A generic article route for linking from non-dynamic parts if needed, or for a default article */}
+          <Route path="/article" element={<ArticlePage />} /> 
+          {/* <Route path="/about" element={<AboutPage />} /> */}
+          {/* Consider adding a 404 Not Found page later */}
+          {/* <Route path="*" element={<NotFoundPage />} /> */}
+        </Routes>
+      </Box>
+      <Footer />
+    </Flex>
   );
 }
 
